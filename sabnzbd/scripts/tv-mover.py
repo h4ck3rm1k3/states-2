871937c183058d18ab452b15e22f35657ed3455d
David Logie <d@djl.im>
2014-04-05 13:09:00 +0100

    Try to work around obfuscated file names.

--- sabnzbd/scripts/tv-mover.py
+++ sabnzbd/scripts/tv-mover.py
@@ -84,46 +84,78 @@ def mkdirp(path, mode=0755):
 def main(job_dir, nzb, clean, index_num, category, group, status):
     config = get_config(CONFIG_FILE)
 
+    # Loop through the files in ``job_dir``, trying to find one which
+    # matches PATTERN. Also build a list of files and their sizes.
+    #
+    # If no files match PATTERN but we do find one or more files with
+    # a suitable extension we assume the largest file is the one we
+    # want. We take this file use the ``job_dir`` as the base for the
+    # new file name.
+    match = None
+    files_by_size = []
     for fn in os.listdir(job_dir):
         ext = fn.rsplit('.', 1)[-1]
         if ext not in EXTENSIONS:
             continue
 
+        abs = os.path.abspath(os.path.join(job_dir, fn))
+        files_by_size.append((fn, os.path.getsize(abs)))
         match = PATTERN.match(fn)
+        if match:
+            break
+
+    files_by_size = sorted(files_by_size, key=lambda x: x[1])
+    original_name = None
+
+    if not match:
+        # no files at all? just exit
+        if not files_by_size:
+            print("No suitable files found")
+            sys.exit()
+
+        # found a potential match
+        fn = files_by_size[0][0]
+        match = PATTERN.match(os.path.basename(job_dir))
         if not match:
-            continue
+            print("No suitable files found")
+            sys.exit()
 
-        fn = os.path.join(job_dir, fn)
-        dotted_name = string.capwords(match.group(1), '.')
-        canonical_name = get_canonical_name(dotted_name)
-        show_dir = os.path.join(config['tv_directory'], canonical_name)
-        season_dir = os.path.join(show_dir, 'Season %s' % int(match.group(2)))
 
-        filename = "%s.s%se%s.%s" % (dotted_name, match.group(2), match.group(3), ext)
-        final_name = os.path.join(season_dir, filename)
-        if os.path.isfile(final_name):
-            print("File already exists: %s" % final_name)
-            sys.exit(1)
+    original_name = fn
+    fn = os.path.join(job_dir, fn)
+    dotted_name = string.capwords(match.group(1), '.')
+    canonical_name = get_canonical_name(dotted_name)
+    show_dir = os.path.join(config['tv_directory'], canonical_name)
+    season_dir = os.path.join(show_dir, 'Season %s' % int(match.group(2)))
 
-        # create the full path to the new location, move the file over
-        # and remove the old directory
-        mkdirp(show_dir)
-        mkdirp(season_dir)
-        shutil.move(fn, final_name)
-        shutil.rmtree(job_dir)
+    filename = "%s.s%se%s.%s" % (dotted_name, match.group(2), match.group(3), ext)
+    final_name = os.path.join(season_dir, filename)
+    if os.path.isfile(final_name):
+        print("File already exists: %s" % final_name)
+        sys.exit(1)
 
-        print("New file: %s" % final_name)
+    # create the full path to the new location, move the file over
+    # and remove the old directory
+    mkdirp(show_dir)
+    mkdirp(season_dir)
+    shutil.move(fn, final_name)
+    shutil.rmtree(job_dir)
 
-        # try to remove the empty category directories
-        parent_dirname = os.path.dirname(os.path.abspath(job_dir))
-        parent_basename = os.path.basename(parent_dirname)
-        if parent_basename.lower() == category.lower():
-            try:
-                os.rmdir(parent_dirname)
-                print("Removed empty directory: %s" % parent_dirname)
-            except OSError:
-                print("Skipped non-empty directory: %s" % parent_dirname)
-                pass
+    msg = "New file: %s" % final_name
+    if original_name is not None:
+        msg = "%s (renamed from %s)" % (msg, original_name)
+    print(msg)
+
+    # try to remove the empty category directories
+    parent_dirname = os.path.dirname(os.path.abspath(job_dir))
+    parent_basename = os.path.basename(parent_dirname)
+    if parent_basename.lower() == category.lower():
+        try:
+            os.rmdir(parent_dirname)
+            print("Removed empty directory: %s" % parent_dirname)
+        except OSError:
+            print("Skipped non-empty directory: %s" % parent_dirname)
+            pass
 
 
 if __name__ == '__main__':
